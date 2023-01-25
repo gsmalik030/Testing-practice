fde3caeba8847a409c34984b3f3405020c4edc6e
Ghulam Subbhani <gsmalik030@gmail.com>
2023-01-25 08:49:24 +0200

    _Solved string Length problem with test

---
+++ string.js
@@ -0,0 +1,13 @@
+function stringLength(string) {
+  if(string===""){
+    return "String can't be empty"
+  }
+  else if(string.length>10){
+    return "Length is more than 10"
+  }
+  else{
+    return string.length;
+  }
+    
+  }
+  module.exports = stringLength;
---
+++ string.test.js
@@ -0,0 +1,13 @@
+const stringLength = require('./string');
+
+test('abcde length to equal 5', () => {
+  expect(stringLength('abcde')).toBe(5);
+});
+
+test('"" throw an error', () => {
+  expect(stringLength('')).toBe("String can't be empty");
+});
+
+test('abcdefghijk length is more then 10', () => {
+  expect(stringLength('abcdefghijk')).toBe("Length is more than 10");
+});
